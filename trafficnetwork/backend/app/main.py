411fa5db335f671286eac32e3af1313379922c37
Jaohar Ali <jaoharlit@gmail.com>
2026-03-05 18:00:27 -0500

    be done

--- trafficnetwork/backend/app/main.py
+++ trafficnetwork/backend/app/main.py
@@ -12,12 +12,14 @@ app = FastAPI(
 # CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "https://traffic-network-rerouting-git-4245a5-jaoharaligithubs-projects.vercel.app",
-                   "https://traffic-network-rerouting-model-benpk0byj.vercel.app",],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# "http://localhost:3000", "http://localhost:5173", "https://traffic-network-rerouting-git-4245a5-jaoharaligithubs-projects.vercel.app",
+#                    "https://traffic-network-rerouting-model-benpk0byj.vercel.app",
 
 app.include_router(routes.router, prefix="/api")
 
